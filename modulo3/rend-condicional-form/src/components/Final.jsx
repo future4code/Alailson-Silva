9133dbdc6ebbf9edaafe1c3da43c6f73285abbfe
Alailson Barbosa <alailsonbcsilva@gmail.com>
2021-11-23 21:19:51 -0300

    exercicio renderizacao-condicional

---
+++ modulo3/rend-condicional-form/src/components/Final.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Final() {
+  return (
+    <div>
+
+        <h1>O formulário acabou!</h1>
+        <br />
+        <h3>Muito obrigado por participar! Entraremos em contato!</h3>
+        
+      
+    </div>
+  );
+}
+
+export default Final;
