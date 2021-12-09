1bc15ffc5edd4896a27afbc197a3baa95b229838
Alailson Barbosa <alailsonbcsilva@gmail.com>
2021-12-09 19:51:57 -0300

    labenusers

---
+++ modulo3/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <button>Trocar tela</button><br />
+      <input type="text" placeholder="Nome"></input>
+      <input type="email" placeholder="Email"></input>
+      <button>Criar usuário</button>
+    </div>
+  );
+}
+
+export default App;
